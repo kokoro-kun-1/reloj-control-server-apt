60615aa577b8cbbaaf098a9659f9486761f8cbf6
kokoro-kun-1 <kokoro-kun-1@users.noreply.github.com>
2026-07-01 03:35:18 +0000

    Publish fixed Debian package 0.1.0-2

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 set -eu
 sudo install -d -m 0755 /etc/apt/sources.list.d
 printf '%s
-' 'deb [trusted=yes] https://raw.githubusercontent.com/kokoro-kun-1/reloj-control-server-apt/main stable main' | sudo tee /etc/apt/sources.list.d/reloj-control-server.list >/dev/null
+' 'deb [trusted=yes] https://raw.githubusercontent.com/kokoro-kun-1/reloj-control-server-apt/main linuxmint main' | sudo tee /etc/apt/sources.list.d/reloj-control-server.list >/dev/null
 sudo apt update
